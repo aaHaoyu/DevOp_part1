d9734966eb3c9d647acbe440b49643550400ddee
aaHaoyu <145046313+aaHaoyu@users.noreply.github.com>
2024-05-31 16:44:03 +0200

    Update test_matrix_multiplication.cpp

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -30,20 +30,22 @@
 
 TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
-        {1, 1},
-        {1, 1}
+        {1, 2},
+        {1, 2},
+        {1, 2}
     };
     std::vector<std::vector<int>> B = {
-        {1, 1},
-        {0, 0}
+        {1},
+        {2}
     };
     std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
 
-    multiplyMatrices(A, B, C, 2, 2, 2);
+    multiplyMatrices(A, B, C, 3, 2, 1);
 
     std::vector<std::vector<int>> expected = {
-        {1, 1},
-        {1, 1}
+        {5},
+        {5},
+        {5}
     };
 
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
